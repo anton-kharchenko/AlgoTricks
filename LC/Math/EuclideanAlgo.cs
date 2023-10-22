ad3dfb8c50aaafc865b423b478e4f60965f0e5d3
Anton Kharchenko <anton.kharchenko.1997@gmail.com>
2023-10-23 00:35:25 +0300

    feat(Euclidean Algo): Gratest Common Divisor, Lowest Common Devisor

---
+++ LC/Math/EuclideanAlgo.cs
@@ -0,0 +1,32 @@
+﻿namespace LC.Math;
+
+public class EuclideanAlgo
+{
+    /// <summary>
+    /// Greatest common divisor (GCD) - GCD(a,b) == GCD(b, a%b)
+    /// Time - O(log min(a, b))
+    /// </summary>
+    public int Gcd(int a, int b)
+    {
+        return b == 0 ? a : Gcd(b, a % b);
+    }
+    
+    /// <summary>
+    /// Lowes common divisor (a*b) / GCD(a, b);
+    /// Time - O((a*b) / log min(a, b))
+    /// </summary>
+    public int Lcm(int a, int b)
+    {
+        return a * b / Gcd(a, b);
+    }
+    
+    
+    /// <summary>
+    /// Check Result
+    /// Time - O((a*b) / log min(a, b))
+    /// </summary>
+    public bool CheckResult(int a, int b)
+    {
+        return a * b == Gcd(a, b) * Lcm(a, b);
+    }
+}
--- LC/Program.cs
+++ LC/Program.cs
@@ -5,8 +5,12 @@
 // Console.WriteLine(sl.RecursiveExponentiation(2, 7));
 // Console.WriteLine("Binary version");
 // Console.WriteLine(sl.BinaryExponentiation(2, 7));
-var sl = new BinarySearch();
-Console.WriteLine(sl.IsExists(new []{1,2,3,4,5,6,8}, 6));
-Console.WriteLine(sl.IsExists(new []{1,2,3,4,5,6,8}, 7));
-
+// var sl = new BinarySearch();
+// Console.WriteLine(sl.IsExists(new []{1,2,3,4,5,6,8}, 6));
+// Console.WriteLine(sl.IsExists(new []{-6,1,2,3,4,5,6,8}, 7));
+// Console.WriteLine(sl.IsExists(new []{-6,1,2,3,4,5,6,8}, -6));
+var sl = new EuclideanAlgo();
+Console.WriteLine(sl.Gcd(15, 21));
+Console.WriteLine(sl.Lcm(15, 21));
+Console.WriteLine(sl.CheckResult(15, 21));
 
