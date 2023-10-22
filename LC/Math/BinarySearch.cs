9ba793e8f036a8551c2d1918f4089c6e9d8c4964
Anton Kharchenko <anton.kharchenko.1997@gmail.com>
2023-10-22 19:21:23 +0300

    feat(math): binary search

---
+++ LC/Math/BinarySearch.cs
@@ -0,0 +1,26 @@
+﻿namespace LC.Math;
+
+public class BinarySearch
+{
+    public bool IsExists(int[] arr, int target)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+        while (left<= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target)
+                return true;
+            if (arr[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
--- LC/Program.cs
+++ LC/Program.cs
@@ -1,7 +1,12 @@
 ﻿using LC.Math;
 
-var sl = new BinaryPow();
-Console.WriteLine("Recursive version");
-Console.WriteLine(sl.RecursiveExponentiation(2, 7));
-Console.WriteLine("Binary version");
-Console.WriteLine(sl.BinaryExponentiation(2, 7));
+// var sl = new BinaryPow();
+// Console.WriteLine("Recursive version");
+// Console.WriteLine(sl.RecursiveExponentiation(2, 7));
+// Console.WriteLine("Binary version");
+// Console.WriteLine(sl.BinaryExponentiation(2, 7));
+var sl = new BinarySearch();
+Console.WriteLine(sl.IsExists(new []{1,2,3,4,5,6,8}, 6));
+Console.WriteLine(sl.IsExists(new []{1,2,3,4,5,6,8}, 7));
+
+
