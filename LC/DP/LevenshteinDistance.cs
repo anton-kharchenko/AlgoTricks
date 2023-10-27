d8d69c639c897ae58a57b67ea67cf010726be247
Anton Kharchenko <anton.kharchenko.1997@gmail.com>
2023-10-28 00:12:37 +0300

    feat(dp): Levenshtein distance

---
+++ LC/DP/LevenshteinDistance.cs
@@ -0,0 +1,31 @@
+﻿namespace LC.DP;
+
+public class LevenshteinDistance
+{
+    public int Difference(string original, string actual)
+    {
+        var board = new int[original.Length + 1, actual.Length + 1];
+
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            board[i, 0] = i;
+        }
+
+        for (int i = 0; i < board.GetLength(1); i++)
+        {
+            board[0, i] = i;
+        }
+
+        for (int i = 1; i < board.GetLength(0); i++)
+        {
+            for (int j = 1; j < board.GetLength(1); j++)
+            {
+                board[i, j] = System.Math.Min(board[i - 1, j] + 1, board[i, j - 1] + 1);
+                var isSame = original[i - 1] == actual[j - 1];
+                board[i, j] = System.Math.Min(board[i, j], board[i - 1, j - 1] + (isSame ? 0 : 1));
+            }
+        }
+
+        return board[original.Length, actual.Length];
+    }
+}
